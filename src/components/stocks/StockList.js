0414b1f429a3a4f761a9b0da1299f0980f2c76fc
Janusz Szubert <theinvalidnonce@gmail.com>
2018-06-02 21:22:47 -0400

    Add StockList component

---
+++ src/components/stocks/StockList.js
@@ -0,0 +1,25 @@
+import React, { Component } from 'react'
+
+//import StockItem
+// Will need a basic form to enter requested stock
+
+class StockList extends Component {
+
+  renderStockList = () => {
+    return this.props.stocks.map( (symbol, idx) => {
+      return <p key={idx}>{symbol}</p> //This will be stockitem component
+    })
+  }
+
+
+  render() {
+    return (
+      <div className='stock-list'>
+        <p>Stock List</p>
+        {this.renderStockList()}
+      </div>
+    )
+  }
+}
+
+export default StockList

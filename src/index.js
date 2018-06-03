777730679b1d8927c5fb9ea422543d71989f595e
Janusz Szubert <theinvalidnonce@gmail.com>
2018-06-02 20:50:12 -0400

    Add header, footer components

--- src/index.js
+++ src/index.js
@@ -3,13 +3,9 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
-import store from './store/store'
-import { Provider } from 'react-redux'
 
 ReactDOM.render(
-          <Provider store={store}>
-            <App />
-          </Provider>, 
+            <App />,
           document.getElementById('root'),
         );
 

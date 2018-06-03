8408dcb3816c601d17d7b6bdbc8ba5718dff29d3
Janusz Szubert <theinvalidnonce@gmail.com>
2018-06-02 21:56:37 -0400

    Edit render routes

--- src/App.js
+++ src/App.js
@@ -6,17 +6,36 @@ import { Route, Switch } from 'react-router-dom'
 import Header from './components/Header'
 // import Footer from './components/Footer'
 import StockList from './components/stocks/StockList'
-import StockDetails from './components/stocks/details/StockDetails'
+import StockInfo from './components/stocks/info/StockInfo'
 
 class App extends Component {
+  constructor(props) {
+    super(props)
+
+    this.state = {
+      stocks: ['GOOG', 'TSLA', 'SPOT', 'FB', 'AAPL']
+    }
+  }
+
   render() {
+    const { stocks } = this.state
+
     return (
       <div className="App">
        <Header />
         <main className='main'>
           <Switch>
-            <Route exact path='/' component={StockList} />
-            <Route exact path='/:symbol' component={StockDetails} />
+            <Route exact path='/' 
+              render={ () => (
+                <StockList stocks={stocks} />
+              )}
+            />
+
+            <Route exact path='/:symbol' 
+              render={ props => (
+                <StockInfo symbol={props.match.params.symbol} />
+              )}
+            />
           </Switch>
         </main>
       </div>
